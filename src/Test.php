19a0e1774cb105b02d4980ae0dcdb87fc052d50e
刘伟亮 <50298602+lwlzq@users.noreply.github.com>
2022-11-17 10:20:32 +0800

    first

---
+++ src/Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @Filename:
+ * @Description:
+ * @Author: liuweiliang
+ * @CreateDate: 2022/11/17 10:18
+ * @UpdateDate: 2022/11/17 10:18 By liuweiliang
+ */
+namespace Gp\Ding;
+
+class Test {
+    public static function test()
+    {
+        var_dump(333);die();
+    }
+}
